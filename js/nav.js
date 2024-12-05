1e0c973f158345d261ef7491ee605fac461456e8
Sleepy <hugo.warnotte@gmail.com>
2024-12-05 16:16:26 +0100

    Rearrangement du main + Ajout de spotify + correction de Palmares

--- js/nav.js
+++ js/nav.js
@@ -2,8 +2,10 @@ window.addEventListener('scroll', function(event) {
     const navigationBar = document.getElementById('nav');
     if (window.scrollY > 0) {
         navigationBar.classList.add('show');
+        console.log('nav')
     }
     else{
     navigationBar.classList.remove('show');
+    console.log('nav')
     }
 });
