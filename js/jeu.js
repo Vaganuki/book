a431d3727e636592b785e62e96aa3ed79f359389
Vaganuki <hugo.warnotte@gmail.com>
2025-01-05 14:49:39 +0100

    Création de la sections "moi" et "contact + Stylisation de la navsecondaire + corrections mineures

--- js/jeu.js
+++
@@ -1,9 +0,0 @@
-var dosbox=new Dosbox({
-    id:"dosbox",
-    onload: function(dosbox){
-        dosbox.run("","");
-    },
-    onrun: function(dosbox, app) {
-        console.log("App '"+app+"' est en court d'exécution");
-    }
-});
