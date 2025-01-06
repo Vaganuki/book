13acf1dd0e0e148f6266c72f0679ef459d4340e8
Sleepy <hugo.warnotte@gmail.com>
2025-01-06 16:42:15 +0100

    Ajout de la fonction theme (clair/sombre/negatif) + corrections + annotations

--- js/nav.js
+++ js/nav.js
@@ -1,11 +1,51 @@
+const navigationBar = document.getElementById('nav');
+
+const themeBtn=document.getElementById('theme');
+const rond=themeBtn.querySelectorAll('*');
+const body=document.querySelector('body');
+let timer=5;
+
+themeBtn.addEventListener('click', (evt)=>{
+    //reset du timer
+    clearTimeout(timer);
+    //début de la foction
+    timer=setTimeout(()=>{
+        if(evt.detail===3){
+            rond.forEach((child)=>{
+                //Easter egg en cas de triple clique
+                body.classList.add('invert');
+            });
+        }
+        else{
+            rond.forEach((child)=>{
+                //Reset de l'easter egg + check du fill
+                body.classList.remove('invert');
+                let fillCheck=child.getAttribute('fill');
+                //Si "l'ampoule" est allumée alors on passe en thème sombre, sinon en thème clair
+                if(fillCheck=='white'){
+                    child.setAttribute('fill','none');
+                    body.classList.add('darkTheme');
+                }
+                else{
+                    child.setAttribute('fill','white');
+                    body.classList.remove('darkTheme');
+                }
+            });
+        }
+    }, 5);
+});
+
 window.addEventListener('scroll', function(event) {
-    const navigationBar = document.getElementById('nav');
     if (window.scrollY > 0) {
         navigationBar.classList.add('show');
-        console.log('nav')
     }
     else{
     navigationBar.classList.remove('show');
-    console.log('nav')
     }
 });
+
+//Cimetière du code "qui pourrait servir plus tard"
+
+                /*child.setAttribute('fill','black');
+                child.setAttribute('stroke','black');
+                body.classList.remove('darkTheme');*/
