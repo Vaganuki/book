908c6969cf872125894b4a2c4b7cc30b3e48b1f6
Sleepy <hugo.warnotte@gmail.com>
2025-01-08 16:37:21 +0100

    Avancée dans le thème sombre + corrections mineures

--- js/nav.js
+++ js/nav.js
@@ -1,66 +1,18 @@
+//Selection de la nav
 const navigationBar = document.getElementById('nav');
 
+//Selection des éléments nécessaires au changements de thèmes
 const themeBtn=document.getElementById('theme');
-const rond=themeBtn.querySelectorAll('*');
-const body=document.querySelector('body');
+const rond=document.getElementById('rondTheme');
+const html=document.querySelector('html');
 const audioDanse=document.getElementById('caramelleDanse');
 let timer=5;
 
+//Selection des éléments du flash
 const flash=document.getElementById('flashbang');
 const audioFlash=document.getElementById('flashAudio');
 
-themeBtn.addEventListener('click', (evt)=>{
-    //reset du timer
-    clearTimeout(timer);
-    //début de la foction
-    timer=setTimeout(()=>{
-        if(evt.detail===5){
-            rond.forEach((child)=>{
-                body.classList.add('wow');
-                audioDanse.play();
-            });
-        }
-        else{
-            body.classList.remove('wow');
-            audioDanse.pause();
-            if(evt.detail===3){
-                rond.forEach((child)=>{
-                    //Easter egg en cas de triple clique
-                    body.classList.add('invert');
-                });
-            }
-            else{
-                rond.forEach((child)=>{
-                    //Reset de l'easter egg + check du fill
-                    body.classList.remove('invert');
-                    let fillCheck=child.getAttribute('fill');
-                    //Si "l'ampoule" est allumée alors on passe en thème sombre, sinon en thème clair
-                    if(fillCheck=='white'){
-                        child.setAttribute('fill','none');
-                        body.classList.add('darkTheme');
-                        flash.classList.remove('hidden');
-                    }
-                    else{
-                        child.setAttribute('fill','white');
-                        body.classList.remove('darkTheme');
-                        flash.classList.add('hidden');
-                    }
-                });
-            }
-        }
-    }, 5);
-});
-
-flash.addEventListener('click',()=>{
-    audioFlash.play();
-    setTimeout(()=>{
-        body.classList.add('flash');
-    },500);
-    setTimeout(()=>{
-        body.classList.remove('flash');
-    }, 8000);
-});
-
+//Animation de la navigation
 window.addEventListener('scroll', function(event) {
     if (window.scrollY > 0) {
         navigationBar.classList.add('show');
@@ -70,8 +22,50 @@ window.addEventListener('scroll', function(event) {
     }
 });
 
-//Cimetière du code "qui pourrait servir plus tard"
+//Bouton du theme Clair/Sombre/Négatif(s)/Disco
+themeBtn.addEventListener('click', (evt)=>{
+    //reset du timer
+    clearTimeout(timer);
+    //début de la foction
+    timer=setTimeout(()=>{
+        if(evt.detail===5){
+            html.classList.add('wow');
+            audioDanse.play();
+        }
+        else{
+            html.classList.remove('wow');
+            audioDanse.pause();
+            if(evt.detail===3){
+                //Easter egg en cas de triple clique
+                html.classList.add('invert');
+            }
+            else{
+                html.classList.remove('invert');
+                    //Reset de l'easter egg + check du fill
+                    let fillCheck=rond.getAttribute('fill');
+                    //Si "l'ampoule" est allumée alors on passe en thème sombre, sinon en thème clair
+                    if(fillCheck=='white'){
+                        rond.setAttribute('fill','none');
+                        html.classList.add('darkTheme');
+                        flash.classList.remove('hidden');
+                    }
+                    else{
+                        rond.setAttribute('fill','white');
+                        html.classList.remove('darkTheme');
+                        flash.classList.add('hidden');
+                    }
+            }
+        }
+    }, 5);
+});
 
-                /*child.setAttribute('fill','black');
-                child.setAttribute('stroke','black');
-                body.classList.remove('darkTheme');*/
+//Fonction de flash
+flash.addEventListener('click',()=>{
+    audioFlash.play();
+    setTimeout(()=>{
+        html.classList.add('flash');
+    },500);
+    setTimeout(()=>{
+        html.classList.remove('flash');
+    }, 8000);
+});
