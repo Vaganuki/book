5f65d0328d418411488b8cc86a17c74c27d5b6fd
Sleepy <hugo.warnotte@gmail.com>
2025-01-22 14:53:25 +0100

    Ajout survol cartes Palmares + annotation anti CSS

--- js/fuckCSS.js
+++ js/fuckCSS.js
@@ -1,19 +1,29 @@
+//BOUTON
 const btn=document.getElementById('fuckCSS');
+//Les balises liens
 const css=document.getElementById('offCSS');
 const norm=document.getElementById('offNORM');
+//INTERRUPTEUR
 let off=false;
-
+//Fonctionnalité au click
 btn.addEventListener('click', ()=>{
+    //Check de l'état de l'interrupteur
     if(off){
+        //Si vrai rallume le css
         css.setAttribute('href','./css/style.css');
         norm.setAttribute('href','./css/normalize.css');
+        //On adapte le texte du bouton
         btn.innerText='Bouton à désactiver le CSS (stp non)';
-        off=false;
+        //Inversion du bouton
+        off=!off;
     }
     else{
+        //Si faux on éteint
         css.removeAttribute('href');
         norm.removeAttribute('href');
+        //On adapte le texte du bouton
         btn.innerText='RALLUME LE CSS !!!!!!! (stp)';
-        off=true;
+        //Inversion de l'interrupteur
+        off=!off;
     }
 });
