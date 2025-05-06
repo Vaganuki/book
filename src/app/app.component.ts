4a3ab694d84442afaadf80290a3285c0bfa5765c
Vaganuki <hugo.warnotte@gmail.com>
2025-05-06 10:54:40 +0200

    quick tests and setups

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MusicComponent} from './components/music/music.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MusicComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
