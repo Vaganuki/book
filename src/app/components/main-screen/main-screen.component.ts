5ad096cf7d0bb9a693ac3e58fdeb8f82d78a121c
Vaganuki <hugo.warnotte@gmail.com>
2025-05-12 16:43:27 +0200

    Main screen content

--- src/app/components/main-screen/main-screen.component.ts
+++ src/app/components/main-screen/main-screen.component.ts
@@ -1,8 +1,13 @@
 import {Component} from '@angular/core';
+import {Button} from 'primeng/button';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-main-screen',
-  imports: [],
+  imports: [
+    Button,
+    RouterLink
+  ],
   templateUrl: './main-screen.component.html',
   styleUrl: './main-screen.component.scss'
 })
