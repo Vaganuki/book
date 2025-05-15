10ee366b7bcc0f91d1748c45f361b620ca42b12e
Vaganuki <hugo.warnotte@gmail.com>
2025-05-15 10:40:27 +0200

    Correction window is undefined

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,6 +1,7 @@
-import {Component, ElementRef, QueryList, ViewChildren} from '@angular/core';
+import {Component, ElementRef, PLATFORM_ID, QueryList, ViewChildren, inject} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {NavItem} from './models/navitem.model';
+import {isPlatformBrowser} from '@angular/common';
 
 @Component({
   selector: 'app-nav',
@@ -9,6 +10,34 @@ import {NavItem} from './models/navitem.model';
   styleUrl: './nav.component.scss'
 })
 export class NavComponent {
+
+  private platformId = inject(PLATFORM_ID);
+
+  //Support des inputs clavier
+  @ViewChildren('navLink', {read: ElementRef}) nav_ItemList!: QueryList<ElementRef>;
+  private indexNav = 0;
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      window.addEventListener('keydown', this.navKeyFocus);
+    }
+  }
+
+  navKeyFocus = (event: KeyboardEvent) => {
+    const navList = this.nav_ItemList.toArray();
+    switch (event.key) {
+      case 'ArrowUp':
+        this.indexNav = this.indexNav - 1 >= 0 ? this.indexNav - 1 : navList.length - 1;
+        navList[this.indexNav].nativeElement.focus();
+        break;
+      case 'ArrowDown':
+        this.indexNav = (this.indexNav + 1) % navList.length;
+        navList[this.indexNav].nativeElement.focus();
+        break;
+      default:
+        break;
+    }
+  }
   routes: NavItem[] = [
     {
       label: 'Accueil',
@@ -39,29 +68,4 @@ export class NavComponent {
       route: 'dev'
     }
   ];
-
-  //Support des inputs clavier
-  @ViewChildren('navLink', {read: ElementRef}) nav_ItemList!: QueryList<ElementRef>;
-  private indexNav = 0;
-
-  ngAfterViewInit() {
-    window.addEventListener('keydown', this.navKeyFocus);
-    console.log(this.nav_ItemList);
-  }
-
-  navKeyFocus = (event: KeyboardEvent) => {
-    const navList = this.nav_ItemList.toArray();
-    switch (event.key) {
-      case 'ArrowUp':
-        this.indexNav = this.indexNav - 1 >= 0 ? this.indexNav - 1 : navList.length - 1;
-        navList[this.indexNav].nativeElement.focus();
-        break;
-      case 'ArrowDown':
-        this.indexNav = (this.indexNav + 1) % navList.length;
-        navList[this.indexNav].nativeElement.focus();
-        break;
-      default:
-        break;
-    }
-  }
 }
